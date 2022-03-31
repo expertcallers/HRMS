492b825fbec0593a371b5aa23f4043142eb812ed
expertcallers <kalesh.cv@expertcalers.com>
2022-03-31 16:19:35 +0530

    31

--- ecpl_hrms/ams/views.py
+++ ecpl_hrms/ams/views.py
@@ -1260,7 +1260,7 @@ def addNewTeam(request): # Test1
         om = request.POST["om"]
         campaign = request.POST["campaign"]
         today = date.today()
-        cam = Campaigns.objects.create(name=campaign,om=om,created_by=usr,created_date = today)
+        cam = Campaigns.objects.create(name=campaign,om=om,created_by=usr)
         cam.save()
         messages.info(request,'Team ' + campaign +' Created Successfully')
         return redirect('/ams/view-all-teams')
@@ -1321,7 +1321,6 @@ def applyLeave(request): # Test1
         e.emp_rm2_id = emp_rm2_id
         e.emp_rm3_id = emp_rm3_id
         rm1_desi = Profile.objects.get(emp_id=emp_rm1_id).emp_desi
-
         if rm1_desi in manager_list:
             e.tl_status = 'Approved'
             e.tl_approval = True
@@ -1664,39 +1663,3 @@ def addLeaveBalance(request):
         return redirect('/ams/')
 
 
-
-def exportMapping(request):
-
-    if request.method == 'GET':
-       
-        response = HttpResponse(content_type='application/ms-excel')
-        response['Content-Disposition'] = 'attachment; filename="mapping.xls"'
-        wb = xlwt.Workbook(encoding='utf-8')
-        ws = wb.add_sheet('Users Data')  # this will make a sheet named Users Data
-        # Sheet header, first row
-        row_num = 0
-        font_style = xlwt.XFStyle()
-        font_style.font.bold = True
-        columns = [
-                    'Date','Emp ID','Emp Name','Attendance','Designation','RM 1','RM 2','RM 3','Team'
-                  ]
-        for col_num in range(len(columns)):
-            ws.write(row_num, col_num, columns[col_num], font_style)  # at 0 row 0 column
-        # Sheet body, remaining rows
-        font_style = xlwt.XFStyle()
-        start = '2022-03-01'
-        end = '2022-03-31'
-
-        rows = EcplCalander.objects.filter(date__range=[start,end]).values_list(
-                'date','emp_id', 'emp_name','att_actual','emp_desi', 'rm1', 'rm2', 'rm3', 'team'
-            )
-      
-        import datetime
-        rows = [[x.strftime("%Y-%m-%d %H:%M") if isinstance(x, datetime.datetime) else x for x in row] for row in
-                rows]
-        for row in rows:
-            row_num += 1
-            for col_num in range(len(row)):
-                ws.write(row_num, col_num, row[col_num], font_style)
-        wb.save(response)
-        return response
