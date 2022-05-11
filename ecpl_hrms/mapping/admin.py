11f61e80f27f5386d4bb5f037cbccbb5fbf245b8
Ibrahim Khan <ibrahim7353khan@gmail.com>
2022-05-11 14:52:05 +0530

    added Import export to Profile Model

--- ecpl_hrms/mapping/admin.py
+++ ecpl_hrms/mapping/admin.py
@@ -5,11 +5,16 @@ from import_export.admin import ImportExportModelAdmin
 
 
 # Register your models here.
+class ProfileResource(resources.ModelResource):
+  class Meta:
+     model = Profile
+     fields = ['id', 'user', 'emp_id', 'emp_name', 'emp_desi', "emp_rm1", 'emp_rm1_id', "emp_rm2", 'emp_rm2_id',
+               "emp_rm3", 'emp_rm3_id', 'emp_process', 'emp_process_id', 'doj']
 
-
-class ProfileSearch(admin.ModelAdmin):
-    search_fields = ('emp_name', 'emp_id', "emp_desi")
+class ProfileSearch(ImportExportModelAdmin):
+    search_fields = ('emp_name', 'emp_id', "emp_desi", 'emp_process')
     list_display = ('emp_name', 'emp_id', 'emp_desi', 'emp_process', "emp_rm1", "emp_rm2", "emp_rm3")
+    resource_class = ProfileResource
 
 
 class MappingSearch(admin.ModelAdmin):
